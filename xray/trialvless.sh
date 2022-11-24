700d72f7d961ac113fa54cbbf3be0789fb30fb4a
Denisuprapto <denisuprapto@upi.edu>
2022-11-24 20:15:23 +0800

    scriptws

---
+++ xray/trialvless.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+#########################
+
+clear
+source /var/lib/SIJA/ipvps.conf
+if [[ "$IP" = "" ]]; then
+domain=$(cat /etc/xray/domain)
+else
+domain=$IP
+fi
+tls="$(cat ~/log-install.txt | grep -w "Vless TLS" | cut -d: -f2|sed 's/ //g')"
+none="$(cat ~/log-install.txt | grep -w "Vless None TLS" | cut -d: -f2|sed 's/ //g')"
+until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
+
+
+		read -rp "User: " -e user
+		CLIENT_EXISTS=$(grep -w $user /etc/xray/config.json | wc -l)
+
+		if [[ ${CLIENT_EXISTS} == '1' ]]; then
+        
+		exit
+		fi
+	done
+
+uuid=$(cat /proc/sys/kernel/random/uuid)
+read -p "Expired (days): " masaaktif
+exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
+sed -i '/#vless$/a\#& '"$user $exp"'\
+},{"id": "'""$uuid""'","email": "'""$user""'"' /etc/xray/config.json
+sed -i '/#vlessgrpc$/a\#& '"$user $exp"'\
+},{"id": "'""$uuid""'","email": "'""$user""'"' /etc/xray/config.json
+vlesslink1="vless://${uuid}@${domain}:$tls?path=/vless&security=tls&encryption=none&type=ws#${user}"
+vlesslink2="vless://${uuid}@${domain}:$none?path=/vless&encryption=none&type=ws#${user}"
+vlesslink3="vless://${uuid}@${domain}:$tls?mode=gun&security=tls&encryption=none&type=grpc&serviceName=vless-grpc&sni=${domain}#${user}"
+
+clear
+echo -e "━━━━━━━━━━━━━━━━━━━━━"
+echo -e "TRIAL XRAY/VLESS ACCOUNT"
+echo -e "━━━━━━━━━━━━━━━━━━━━━"
+echo -e "Remarks        : ${user}"
+echo -e "Domain         : ${domain}"
+echo -e "Port TLS       : $tls"
+echo -e "Port none TLS  : $none"
+echo -e "id             : ${uuid}"
+echo -e "Encryption     : none"
+echo -e "Network        : ws"
+echo -e "Path           : /vless"
+echo -e "Path           : vless-grpc"
+echo -e "━━━━━━━━━━━━━━━━━━━━━"
+echo -e "Expired In     : 30 Minutes"
+echo -e "━━━━━━━━━━━━━━━━━━━━━"
+echo -e "Link TLS       : ${vlesslink1}"
+echo -e "━━━━━━━━━━━━━━━━━━━━━"
+echo -e "Link none TLS  : ${vlesslink2}"
+echo -e "━━━━━━━━━━━━━━━━━━━━━"
+echo -e "Link GRPC      : ${vlesslink3}"
+echo -e "━━━━━━━━━━━━━━━━━━━━━"
+echo -e "THANKS FOR USING OUR SERVICE"
+sleep 1
+systemctl restart xray > /dev/null 2>&1
